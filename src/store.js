3f4d257acb17f0eeda954751b1c276795bedbd5f
aimeetacchi <aimeetacchi@hotmail.co.uk>
2021-09-19 22:21:49 +0100

    Changed store so it doesnt show redux dev tools if u are on phone

--- src/store.js
+++ src/store.js
@@ -5,12 +5,16 @@ import thunk from 'redux-thunk'
 
 const initialState = {}
 
+const middleware = [
+    applyMiddleware(thunk),
+    ...(window.__REDUX_DEVTOOLS_EXTENSION__ ? [window.__REDUX_DEVTOOLS_EXTENSION__()] : [])
+  ]
+
 const store = createStore(
     reducers,
     initialState,
     compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        ...middleware
     )
 );
 
